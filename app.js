7cf0460525b71ed0620e7f7ac2740da5e6aacc5e
Oscar Forss <oscar.forss.1@gmail.com>
2021-03-23 13:18:45 +0100

    code clean up, change selectors

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 console.log("app.js running...");
 
 const products = [];
-const $totalCart = $('#in-cart');
+const totalCart = $('#in-cart');
 
 $(document).ready(() => {
 
     $('.cart-dropdown').hide();
     $('.drop-cart-clear').click(() => {
         localStorage.clear()
-        $totalCart.html('0')
+        totalCart.html('0')
         $('#drop-cart-total-price').html('0')
         $('.cart-dropdown').hide()
         renderDropCartItem()
@@ -48,17 +48,30 @@ $(document).ready(() => {
     }))
 
     function addFuctionToAllProductCardButtons(e) {
-        // detta är för att vi ska hämta rätt element i arrayen med produkter
-        // därför tar vi knapp id -1 
        cartNumbers("add");
-       // lägg till productens värde i totalsumman
        totalSumOfCart(products[e.id -1]);
-       // spara proucter till kundvagnen
        addProductToCart(products[e.id -1]);
-       
     }
 
-    // to save products in cart to local strage
+    function cartNumbers(subtractAdd){
+        let productNumbers = parseInt(localStorage.getItem('cartNumbers'));
+        if (productNumbers) {
+            if(subtractAdd === "add"){
+                localStorage.setItem('cartNumbers', productNumbers + 1);
+            }else if (subtractAdd === "subtract"){
+                localStorage.setItem('cartNumbers', productNumbers - 1);
+            }
+        }else {
+        localStorage.setItem('cartNumbers', 1);
+        }
+        updateCartUI();
+    }
+
+    function totalSumOfCart(product){
+        let price = localStorage.getItem('totalPriceOfCart');
+        price != null ? localStorage.setItem('totalPriceOfCart', parseInt(price) + product.price) : localStorage.setItem('totalPriceOfCart', product.price);
+    } 
+
     function addProductToCart(product){
         let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
         if(cartItems != null){
@@ -84,72 +97,35 @@ $(document).ready(() => {
 
     function renderDropCartItem() {
         let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
-        let dropCart = document.querySelector('.line-item-container');
+        let dropCart = $('.line-item-container');
         if(cartItems && dropCart){
-            dropCart.innerHTML = '';
-        // här sätter vi id på cart-product till den aktuellt 
-        // productens id så vi har tillgång till den senare
-        Object.values(cartItems).map(e => {
-            dropCart.innerHTML +=` 
+            dropCart.html("");
+        $.each(cartItems, (index, e) => {
+            dropCart.append(` 
             <div class="line-item">
                       <p class="cart-1 drop-product">${e.productName}</p>
                       <p class="cart-2 drop-quantity">${e.inCart}</p>
                       <p class="cart-3 drop-a-price">${e.price}</p>
                       <p class="cart-4 drop-total-price-">${e.price * e.inCart}</p>
             </div>
-            `
-        });
+            `)
+        })
         $('#drop-cart-total-price').html(JSON.parse(localStorage.getItem('totalPriceOfCart')))
         }else{
-            dropCart.innerHTML = '';
-        }
-        
-    }
-
-    function cartNumbers(subtractAdd){
-        // vi får en sträng från local storage för att kunna lägga till en 
-        // vara behöver vi parsa den till int
-        let $productNumbers = parseInt(localStorage.getItem('cartNumbers'));
-        // kollar om de finns något i cart om de finns lägger vi till 1 till värdet, annars lägger vi in 1
-        if ($productNumbers) {
-            if(subtractAdd === "add"){
-                localStorage.setItem('cartNumbers', $productNumbers + 1);
-                $totalCart.val($productNumbers + 1);
-            }else if (subtractAdd === "subtract"){
-                localStorage.setItem('cartNumbers', $productNumbers - 1);
-                $totalCart.val($productNumbers - 1);
-            }
-        }else {
-        localStorage.setItem('cartNumbers', 1);
-        $totalCart.val(0);
-        //document.querySelector('.cart span').textContent = 0;
+            dropCart.html("");
         }
     }
-
-    // to save totalprice of cart to local storage
-    function totalSumOfCart(product){
-        let price = localStorage.getItem('totalPriceOfCart');
-        price != null ? localStorage.setItem('totalPriceOfCart', parseInt(price) + product.price) : localStorage.setItem('totalPriceOfCart', product.price);
-    } 
-    
+ 
     $('.cart').click(() => $('.cart-dropdown').slideToggle(300)) 
 
-    $('.go-to-cart').click(() => window.location.href = "./cart/")
-
-    renderDropCartItem();
-    
+    $('.go-to-cart').click(() => window.location.href = "./cart/") 
 })
 
-// to update cart UI when page loads
 function updateCartUI(){
     let productNumbers = parseInt(localStorage.getItem('cartNumbers'));
-    //productNumbers ? document.querySelector('.cart span').textContent = productNumbers : document.querySelector('.cart span').textContent = 0;
-    productNumbers ? $totalCart.html(productNumbers):$totalCart.html(0);
+    productNumbers ? totalCart.html(productNumbers):totalCart.html(0);
 } 
 
-
-// add function to all product cards buttons
-//localStorage.setItem('productList', JSON.stringify(products));
 window.onload = () => { 
     updateCartUI();
 }
--- cart/cart.js
+++ cart/cart.js
@@ -2,18 +2,15 @@ console.log("cart.js running...");
 
 function renderCart(){
     let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
-    let cartProducts = document.querySelector('.cart-products');
-
+    let cartProducts = $('.cart-products');
+    
     if(cartItems === null){
-        document.getElementById('total-price').textContent = "0";
+        $('#total-price').val("0");
     }
-
     if(cartItems && cartProducts){
-        cartProducts.innerHTML = '';
-        // här sätter vi id på cart-product till den aktuellt 
-        // productens id så vi har tillgång till den senare
-        Object.values(cartItems).map(e => {
-            cartProducts.innerHTML +=` 
+         cartProducts.html("");
+        $.each(cartItems, (index, e) => {
+            cartProducts.append(` 
             <div class="cart-product" id="${e.id}">
                 <img src="${e.image}" class="cart-img">
                 <span class="cart-product-name cart-font-size">${e.productName}</span>
@@ -25,41 +22,35 @@ function renderCart(){
                 </div>
                 <div class="cart-total cart-font-size">${e.price * e.inCart}</div>
             </div>
-            `
-        });
+            `)
+        })
 
-        const addBtns = document.querySelectorAll('.add-product-to-cart');
-        const removeBtns = document.querySelectorAll('.remove-product-to-cart');
+        $.each($('.add-product-to-cart'),function( index, value ) {
+            value.addEventListener('click',(e) => {
+            addProductToCart(cartItems[e.target.parentElement.parentElement.id]);
+            totalSumOfCart(cartItems[e.target.parentElement.parentElement.id], "add");
+            cartNumbers(cartItems[e.target.parentElement.parentElement.id],"add");
+            updateTotalPriceUI();
+            renderCart();
+            })
+          })
 
-        addBtns.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                addProductToCart(cartItems[e.target.parentElement.parentElement.id]);
-                totalSumOfCart(cartItems[e.target.parentElement.parentElement.id], "add");
-                cartNumbers(cartItems[e.target.parentElement.parentElement.id],"add");
-                updateCartUI();
-                updateTotalPriceUI();
-                renderCart();
-            });
-        });
-        removeBtns.forEach(btn => {
-            btn.addEventListener('click', (e) => {
+          $.each($('.remove-product-to-cart'),function( index, value ) {
+            value.addEventListener('click',(e) => {
                 if(cartItems[e.target.parentElement.parentElement.id].inCart !== 0){
-                removeProductFromCart(cartItems[e.target.parentElement.parentElement.id]);
-                totalSumOfCart(cartItems[e.target.parentElement.parentElement.id], "subtract");
-                cartNumbers(cartItems[e.target.parentElement.parentElement.id],"subtract");
-                updateCartUI();
-                updateTotalPriceUI();
-                renderCart();
-                }      
-            });
-        });        
+                    removeProductFromCart(cartItems[e.target.parentElement.parentElement.id]);
+                    totalSumOfCart(cartItems[e.target.parentElement.parentElement.id], "subtract");
+                    cartNumbers(cartItems[e.target.parentElement.parentElement.id],"subtract");
+                    updateTotalPriceUI();
+                    renderCart();
+            }
+          })
+        })
     }
 } 
 
-// to save products in cart to local strage
 function addProductToCart(product){
     let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
-    
     if(cartItems != null){
             cartItems[product.id].inCart += 1;
             cartNumbers("add");
@@ -68,8 +59,7 @@ function addProductToCart(product){
   }
 
 function removeProductFromCart(product){
-    let cartItems = JSON.parse(localStorage.getItem('productsInCart'));
-    
+    let cartItems = JSON.parse(localStorage.getItem('productsInCart'));   
     if(cartItems != 0){
             cartItems[product.id].inCart -= 1;
             cartNumbers("subtract");
@@ -78,34 +68,24 @@ function removeProductFromCart(product){
   }
 
 function cartNumbers(subtractAdd){
-    // vi får en sträng från local storage för att kunna lägga till en 
-    // vara behöver vi parsa den till int
     let productNumbers = parseInt(localStorage.getItem('cartNumbers'));
-    // kollar om de finns något i cart om de finns lägger vi till 1 till värdet, annars lägger vi in 1
-    if (productNumbers) {
+   if (productNumbers) {
         if(subtractAdd === "add"){
             localStorage.setItem('cartNumbers', productNumbers + 1);
-            document.querySelector('.cart span').textContent = productNumbers + 1;
         }else if (subtractAdd === "subtract"){
             localStorage.setItem('cartNumbers', productNumbers - 1);
-            document.querySelector('.cart span').textContent = productNumbers - 1;
-            if(localStorage.getItem('cartNumbers') == 0){
-                document.querySelector('.cart span').textContent = 0;
-            }
         }
     }else {
         if(subtractAdd === "add"){
             localStorage.setItem('cartNumbers', productNumbers + 1);
-            document.querySelector('.cart span').textContent = productNumbers + 1;
         }
     }
+    updateCartUI();
 };
 
-// to save totalprice of cart to local storage
 function totalSumOfCart(product, subtractAdd){
     let price = localStorage.getItem('totalPriceOfCart');
     if(price != null){
-        // convert localstorage from string to number
         if(subtractAdd === "add"){
             price = parseFloat(price);
             localStorage.setItem('totalPriceOfCart', price + product.price);
@@ -120,27 +100,27 @@ function totalSumOfCart(product, subtractAdd){
     }    
 }
 
-const clearBtn = document.getElementById('clear-cart-btn');
-clearBtn.addEventListener('click', () => {
-    localStorage.clear();
-    location.reload();
-});
-
-// to update cart UI when page loads
 function updateCartUI(){
   let productNumbers = parseInt(localStorage.getItem('cartNumbers'));
-  productNumbers ? document.querySelector('.cart span').textContent = productNumbers : document.querySelector('.cart span').textContent = 0;
+  let cartSpan = $('.cart span');
+  productNumbers ? cartSpan.text(productNumbers) : cartSpan.text(0);
 }
 
 function updateTotalPriceUI(){
     let price = parseFloat(localStorage.getItem('totalPriceOfCart'));
-    let totalPrice = document.querySelector('.total-cart-price-number');
+    let totalPrice = $('#total-price');
+    
     if(!price){
-        totalPrice.textContent = 0;
+       totalPrice.text("0");
     }else {
-        totalPrice.textContent = price;
+        totalPrice.text(price)
     }
 } 
+
+$("#clear-cart-btn").click(() => {
+    localStorage.clear();
+    location.reload();
+})
 
 window.onload = () => {
   renderCart();
